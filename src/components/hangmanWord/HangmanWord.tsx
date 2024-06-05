190f85e02d431d9cbe191e7b2505da9a722c993f
Gloria Rangel <norimaqidoe@hotmail.com>
2024-06-04 21:15:21 -0600

    Conditional rendering for guessed letters

--- src/components/hangmanWord/HangmanWord.tsx
+++ src/components/hangmanWord/HangmanWord.tsx
@@ -1,6 +1,23 @@
+import './hangWord_styles.css'
+
 export function HangmanWord () {
+    const word = 'GUITAR'
+    const guessedLetters = ['U', 'R', 'T']
     return (  
-        <h1>abcdef</h1>
+        <>
+            <div id="hang_word">{word.split("").map((letter, index) => (
+                <span 
+                    id='word_cont'
+                    key={index}
+                >
+                    {
+                        guessedLetters.includes(letter)
+                            ? (<span id='visible'>{letter}</span>)
+                            : (<span id='hidden'>{letter}</span>)
+                    }
+                </span>
+            ))}</div>
+        </>
     );
 }
 
